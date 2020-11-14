f56eb20169cf871a6c46aa9060f818ccec60a312
hushui502 <502131010@qq.com>
2020-11-14 23:50:40 +0800

    remove bullshi*

--- rust/target/debug/deps/rand_core-630a4b60d057c0a0.d
+++
@@ -1,7 +0,0 @@
-D:\program\rustproject\target\debug\deps\rand_core-630a4b60d057c0a0.rmeta: C:\Users\hufan\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_core-0.3.1\src\lib.rs
-
-D:\program\rustproject\target\debug\deps\librand_core-630a4b60d057c0a0.rlib: C:\Users\hufan\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_core-0.3.1\src\lib.rs
-
-D:\program\rustproject\target\debug\deps\rand_core-630a4b60d057c0a0.d: C:\Users\hufan\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_core-0.3.1\src\lib.rs
-
-C:\Users\hufan\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_core-0.3.1\src\lib.rs:
--- rust/target/debug/deps/rustproject.d
+++
@@ -1,5 +0,0 @@
-D:\program\rustproject\target\debug\deps\rustproject.exe: src\main.rs
-
-D:\program\rustproject\target\debug\deps\rustproject.d: src\main.rs
-
-src\main.rs:
