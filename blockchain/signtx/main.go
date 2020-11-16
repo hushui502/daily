7334ea493ab563b008ecf30ee72d5ca281fa7546
hushui502 <502131010@qq.com>
2020-11-16 15:47:02 +0800

    design

--- blockchain/signtx/main.go
+++ blockchain/signtx/main.go
@@ -1,12 +1,15 @@
 package main
+
 import (
 	"context"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rlp"
 	"math/big"
 )
 
@@ -59,3 +62,31 @@ func main() {
 	fmt.Println("send transaction success,tx: ", sigTransaction.Hash().Hex())
 }
 
+// https://cloud.tencent.com/developer/article/1665125
+func design() {
+	// 还原交易对象
+	encodedTxStr :=
+		"0xf889188504a817c800832dc6c09405e56888360ae54acf2a389bab39bd41e3934d2b80a4ee919d50000000000000000000000000000000000000000000000000000000000000007b25a041c4a2eb073e6df89c3f467b3516e9c313590d8d57f7c217fe7e72a7b4a6b8eda05f20a758396a5e681ce1ab4cec749f8560e28c9eb91072ec7a8acc002a11bb1d"
+	encodedTx, err := hexutil.Decode(encodedTxStr)
+	if err != nil {
+		fmt.Println("hexutil.Decode failed: ", err.Error())
+		return
+	}
+	// rlp解码
+	tx := new(types.Transaction)
+	if err := rlp.DecodeBytes(encodedTx, tx); err != nil {
+		fmt.Println("rlp.DecodeBytes failed: ", err.Error())
+		return
+	}
+	// chainId为1的EIP155签名器
+	signer := types.NewEIP155Signer(big.NewInt(1))
+	// 使用签名器从已签名的交易中还原账户公钥
+	from, err := types.Sender(signer, tx)
+	if err != nil {
+		fmt.Println("types.Sender: ", err.Error())
+		return
+	}
+	fmt.Println("from: ", from.Hex())
+	jsonTx, _ := tx.MarshalJSON()
+	fmt.Println("tx: ", string(jsonTx))
+}
