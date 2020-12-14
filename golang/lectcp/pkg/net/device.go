8cdc4cb0ccc5ce403f681b505d036b000bf768ad
hushui502 <502131010@qq.com>
2020-12-14 23:20:31 +0800

    net package

---
+++ golang/lectcp/pkg/net/device.go
@@ -0,0 +1,128 @@
+package net
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"sync"
+)
+
+type LinkDeviceCallbackHandler func(link LinkDevice, protocol EthernetType, payload []byte, src, dst HardwareAddress)
+
+type LinkDevice interface {
+	Type() HardwareType
+	Name() string
+	Address() HardwareAddress
+	BroadcastAddress() HardwareAddress
+	MTU() int
+	HeaderSize() int
+	NeedARP() bool
+	Close()
+	Read(data []byte) (int, error)
+	RxHandler(frame []byte, callback LinkDeviceCallbackHandler)
+	Tx(proto EthernetType, data []byte, dst []byte) error
+}
+
+type Device struct {
+	LinkDevice
+	errors chan error
+	ifaces []ProtocolInterface
+	sync.RWMutex
+}
+
+var devices = sync.Map{}
+
+func RegisterDevice(link LinkDevice) (*Device, error) {
+	if _, exists := devices.Load(link); exists {
+		return nil, fmt.Errorf("link device %s is already registered", link.Name())
+	}
+	dev := &Device{
+		LinkDevice: link,
+		errors:     make(chan error),
+	}
+
+	// launch rx loop
+	go func() {
+		var buf = make([]byte, dev.HeaderSize()+dev.MTU())
+		for {
+			n, err := dev.Read(buf)
+			if n > 0 {
+				dev.RxHandler(buf[:n], rxHandler)
+			}
+
+			if err != nil {
+				dev.errors <- err
+				break
+			}
+		}
+		close(dev.errors)
+	}()
+	
+	devices.Store(link, dev)
+	
+	return dev, nil
+}
+
+func rxHandler(link LinkDevice, protocol EthernetType, payload []byte, src, dst HardwareAddress) {
+	protocols.Range(func(k, v interface{}) bool {
+		var (
+			Type = k.(EthernetType)
+			entry = v.(*entry)
+		)
+
+		if Type == EthernetType(protocol) {
+			dev, ok := devices.Load(link)
+			if !ok {
+				panic("device not found")
+			}
+
+			entry.rxQueue <- &packet{
+				dev:  dev.(*Device),
+				data: payload,
+				src:  src,
+				dst:  dst,
+			}
+
+			return false
+		}
+
+		return true
+	})
+}
+
+func Devices() []*Device {
+	ret := []*Device{}
+	devices.Range(func(_, v interface{}) bool {
+		ret = append(ret, v.(*Device))
+		return true
+	})
+	return ret
+}
+
+func (d *Device) Interfaces() []ProtocolInterface {
+	d.RLock()
+	ret := make([]ProtocolInterface, len(d.ifaces))
+	for i, iface := range d.ifaces {
+		ret[i] = iface
+	}
+	d.RUnlock()
+
+	return ret
+}
+
+func (d *Device) ShutDown() {
+	d.LinkDevice.Close()
+	if err := <-d.errors; err != nil {
+		if !errors.Is(err, io.EOF) {
+			log.Println(err)
+		}
+	}
+	devices.Delete(d.LinkDevice)
+}
+
+func (d *Device) RegisterInterface(iface ProtocolInterface) {
+	d.Lock()
+	d.ifaces = append(d.ifaces, iface)
+	d.Unlock()
+}
---
+++ golang/lectcp/pkg/net/enums.go
@@ -0,0 +1,61 @@
+package net
+
+type HardwareType uint16
+
+const (
+	HardwareTypeLoopback HardwareType = 0x0000
+	HardwareTypeEthernet HardwareType = 0x0001
+)
+
+func (t HardwareType) String() string {
+	switch t {
+	case HardwareTypeLoopback:
+		return "Loopback"
+	case HardwareTypeEthernet:
+		return "Ethernet"
+	default:
+		return "Unknown"
+	}
+}
+
+type EthernetType uint16
+
+const (
+	EthernetTypeIP EthernetType = 0x0800
+	EthernetTypeARP EthernetType = 0x0806
+	EthernetTypeIPv6 EthernetType = 0x86dd
+)
+
+func (t EthernetType) String() string {
+	switch t {
+	case EthernetTypeIP:
+		return "IP"
+	case EthernetTypeARP:
+		return "ARP"
+	case EthernetTypeIPv6:
+		return "IPv6"
+	default:
+		return "Unknown"
+	}
+}
+
+type ProtocolNumber uint8
+
+const (
+	ProtocolNumberICMP ProtocolNumber = 0x01
+	ProtocolNumberTCP ProtocolNumber = 0x06
+	ProtocolNumberUDP ProtocolNumber = 0x11
+)
+
+func (t ProtocolNumber) String() string {
+	switch t {
+	case ProtocolNumberICMP:
+		return "ICMP"
+	case ProtocolNumberTCP:
+		return "TCP"
+	case ProtocolNumberUDP:
+		return "UDP"
+	default:
+		return "Unknown"
+	}
+}
