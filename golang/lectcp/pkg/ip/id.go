5a034c4481b0948a59ae6ba676903c04b13d11aa
hushui502 <502131010@qq.com>
2020-12-15 19:26:45 +0800

    ip

---
+++ golang/lectcp/pkg/ip/id.go
@@ -0,0 +1,19 @@
+package ip
+
+import "sync"
+
+type idManager struct {
+	num uint16
+	mutex sync.Mutex
+}
+
+func (id *idManager) next() uint16 {
+	id.mutex.Lock()
+	defer id.mutex.Unlock()
+
+	id.num += 1
+
+	return id.num
+}
+
+var idm = &idManager{}
