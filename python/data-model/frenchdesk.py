23d6841287ebfc6c39e01d0e9d60ebfc69cbefae
hushui502 <502131010@qq.com>
2020-12-28 11:29:12 +0800

    py init

---
+++ python/data-model/frenchdesk.py
@@ -0,0 +1,20 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rand', 'suit'])
+
+
+class FrenchDesk:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, item):
+        return self._cards[item]
+
+
