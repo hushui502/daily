edab94aa11fef05d79d518c2a4e8aa2921f4d646
hushui502 <502131010@qq.com>
2021-01-18 21:48:28 +0800

    lua chunk

---
+++ golang/luago/binchunk/binary_chunk.go
@@ -0,0 +1,79 @@
+package main
+
+const (
+	LUA_SIGNATURE    = "\x1bLua"
+	LUAC_VERSION     = 0x51
+	LUAC_FORMAT      = 0
+	LUAC_DATA        = "\x19\x93\r\n\x1a\n"
+	CINT_SIZE        = 4
+	CSIZET_SIZE      = 8
+	INSTRUCTION_SIZE = 4
+	LUA_INTEGER_SIZE = 8
+	LUA_NUMBER_SIZE  = 8
+	LUAC_INT         = 0x5678
+	LUAC_NUM         = 370.5
+)
+
+const (
+	TAG_NIL       = 0x00
+	TAG_BOOLEAN   = 0x01
+	TAG_NUMBER    = 0x03
+	TAG_INTEGER   = 0x13
+	TAG_SHORT_STR = 0x04
+	TAG_LONG_STR  = 0x14
+)
+
+type binaryChunk struct {
+	header
+	sizeUpvalues byte
+	mainFunc     *Prototype
+}
+
+type header struct {
+	signature       [4]byte // 魔数
+	version         byte    // 版本号5.3.4 = 5*16+3
+	format          byte    // 格式号，官方默认00
+	luaData         [6]byte // lua发行时间1993 + 回车符0D + 换行符0A
+	cintSize        byte
+	sizetSize       byte
+	instructionSize byte
+	luaIntegerSize  byte //
+	luaNumberSize   byte
+	luacInt         int64   // 整数占8个字节
+	luacNum         float64 // 浮点数8
+}
+
+// function prototype
+type Prototype struct {
+	Source          string // debug
+	LineDefined     uint32
+	LastLineDefined uint32
+	NumParams       byte
+	IsVararg        byte
+	MaxStackSize    byte
+	Code            []uint32
+	Constants       []interface{}
+	Upvalues        []Upvalue
+	Protos          []*Prototype
+	LineInfo        []uint32 // debug
+	LocVars         []LocVar // debug
+	UpvalueNames    []string // debug
+}
+
+type Upvalue struct {
+	Instack byte
+	Idx     byte
+}
+
+type LocVar struct {
+	VarName string
+	StartPC uint32
+	EndPC   uint32
+}
+
+func Undump(data []byte) *Prototype {
+	reader := &reader{}
+	reader.checkHeader()
+	reader.readByte()
+	return reader.readProto("")
+}
