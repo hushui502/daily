5550e3f497d823770233c30d52247d462f5a3854
hushui502 <502131010@qq.com>
2021-01-19 23:55:17 +0800

    bfs cases

---
+++ leetcode/c19.go
@@ -0,0 +1,40 @@
+package leetcode
+
+// 最大深度-递归
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+// 最大深度-广度
+func maxDepth2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{}
+	queue = append(queue, root)
+	ans := 0
+	for len(queue) != 0 {
+		// 每次size都是当前层的node个数
+		size := len(queue)
+		for size > 0 {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			size--
+		}
+		// 当前层的node遍历结束
+		ans++
+	}
+
+	return ans
+}
+
+
