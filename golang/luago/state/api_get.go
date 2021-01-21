202b9cd3e5b8f26f606066a66b333a25e1441fd5
hushui502 <502131010@qq.com>
2021-01-22 02:09:08 +0800

    DIY PL:the part of table

---
+++ golang/luago/state/api_get.go
@@ -0,0 +1,51 @@
+package state
+
+import . "luago/api"
+
+// 创建一个table并且推入栈顶
+func (self *luaState) CreateTable(nArr, nRec int) {
+	t := newLuaTable(nArr, nRec)
+	self.stack.push(t)
+}
+
+func (self *luaState) NewTable() {
+	self.CreateTable(0, 0)
+}
+
+func (self *luaState) GetTable(idx int) LuaType {
+	// 获取一个table
+	t := self.stack.get(idx)
+	// 弹出栈顶=键元素
+	k := self.stack.pop()
+
+	return self.getTable(t, k)
+}
+
+func (self *luaState) getTable(t, k luaValue) LuaType {
+	// t必须为table
+	if tbl, ok := t.(*luaTable); ok {
+		v := tbl.get(k)
+		// 将值推入栈顶
+		self.stack.push(v)
+		return typeOf(v)
+	}
+	panic("not a table!")
+}
+
+func (self *luaState) GetFiled(idx int, k string) LuaType {
+	t := self.stack.get(idx)
+	return self.getTable(t, k)
+}
+
+//func (self *luaState) GetFiled(idx int, k string) LuaType {
+//	self.PushString(k)
+//	return self.GetTable(idx)
+//}
+
+// 键是数字，主要用于table中的数组arr
+func (self *luaState) GetI(idx int, i int64) LuaType {
+	t := self.stack.get(idx)
+	return self.getTable(t, i)
+}
+
+
---
+++ golang/luago/state/api_set.go
@@ -0,0 +1,30 @@
+package state
+
+func (self *luaState) SetTable(idx int) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	k := self.stack.pop()
+
+	self.setTable(t, k, v)
+}
+
+func (self *luaState) setTable(t, k, v interface{}) {
+	if tbl, ok := t.(*luaTable); ok {
+		tbl.put(k, v)
+		return
+	}
+	panic("not a table")
+}
+
+func (self *luaState) SetField(idx int, k string) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	self.setTable(t, k, v)
+}
+
+func (self *luaState) SetI(idx int, i int64) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	self.setTable(t, i, v)
+}
+
