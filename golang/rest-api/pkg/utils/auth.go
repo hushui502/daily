717bf30070e2e1f6aa38bbe42c219fd8ede43360
hushui502 <502131010@qq.com>
2021-03-07 23:13:34 +0800

    add auth test

--- golang/rest-api/pkg/utils/auth.go
+++ golang/rest-api/pkg/utils/auth.go
@@ -2,9 +2,25 @@ package utils
 
 import (
 	"context"
+	"rest-api/pkg/httperrors"
 	"rest-api/pkg/logger"
 )
 
-func ValidateIsOwner(ctx context.Context, creatorID string, logger logger.Logger) {
-	user, err := Get
+func ValidateIsOwner(ctx context.Context, creatorID string, logger logger.Logger) error {
+	user, err := GetUserFromCtx(ctx)
+	if err != nil {
+		return err
+	}
+
+	if user.UserID.String() != creatorID {
+		logger.Errorf(
+			"ValidateIsOwner, userID: %v, creatorID: %V",
+			user.UserID.String(),
+			creatorID,
+		)
+
+		return httperrors.Forbidden
+	}
+
+	return nil
 }
