d5eea64d51a778c19763711029ce14f047f066e5
hushui502 <502131010@qq.com>
2021-06-13 13:57:05 +0800

    eg-rs:gap-buffer

--- rust/rustlv/src/lib.rs
+++ rust/rustlv/src/lib.rs
@@ -62,10 +62,12 @@
 // }
 
 
+
+
+//                                          binary-tree
+// An ordered collection of 'T's
 use std::io::empty;
 
-//                              binary-tree
-// An ordered collection of 'T's
 enum BinaryTree<T> {
     Empty,
     NonEmpty(Box<TreeNode<T>>),
@@ -585,3 +587,263 @@ mod formatting {
     }
 }
 
+
+
+
+//                              gap-buffer
+mod gap {
+    use std;
+    use std::ops::Range;
+
+    pub struct GapBuffer<T> {
+        storage: Vec<T>,
+        gap: Range<usize>
+    }
+
+    impl<T> GapBuffer<T> {
+        pub fn new() -> GapBuffer<T> {
+            GapBuffer { storage: Vec::new(), gap: 0..0 }
+        }
+
+        pub fn capacity(&self) -> usize {
+            self.storage.capacity()
+        }
+
+        pub fn len(&self) -> usize {
+            self.capacity() - self.gap.len()
+        }
+
+        pub fn position(&self) -> usize {
+            self.gap.start
+        }
+
+        unsafe fn space(&self, index: usize) -> *const T {
+            self.storage.as_ptr().offset(index as isize)
+        }
+
+        unsafe fn space_mut(&mut self, index: usize) -> *mut T {
+            self.storage.as_mut_ptr().offset(index as isize)
+        }
+
+        fn index_to_raw(&self, index: usize) -> usize {
+            if index < self.gap.start {
+                index
+            } else {
+                index + self.gap.len()
+            }
+        }
+
+        pub fn get(&self, index: usize) -> Option<&T> {
+            let raw = self.index_to_raw(index);
+            if raw < self.capacity() {
+                unsafe {
+                    Some(&*self.space(raw))
+                }
+            } else {
+                None
+            }
+        }
+
+        pub fn set_position(&mut self, pos: usize) {
+            if pos > self.len() {
+                panic!("index {} out of range for GapBuffer", pos);
+            }
+
+            unsafe {
+                let gap = self.gap.clone();
+                if pos > gap.start {
+                    let distance = pos - gap.start;
+                    std::ptr::copy(self.space(gap.end),
+                                    self.space_mut(gap.start),
+                                        distance);
+                } else if pos < gap.start {
+                    let distance = gap.start - pos;
+                    std::ptr::copy(self.space(pos),
+                                   self.space_mut(gap.end - distance),
+                                   distance);
+                }
+                self.gap = pos .. pos + gap.len();
+            }
+        }
+
+        pub fn insert(&mut self, elt: T) {
+            if self.gap.len() == 0 {
+                self.enlarge_gap()
+            }
+
+            unsafe {
+                let index = self.gap.start;
+                std::ptr::write(self.space_mut(index), elt)
+            }
+            self.gap.start += 1;
+        }
+
+        pub fn insert_iter<I>(&mut self, iterable: I)
+            where I: IntoIterator<Item = T>
+        {
+            for item in iterable {
+                self.insert(item)
+            }
+        }
+
+        pub fn remove(&mut self) -> Option<T> {
+            if self.gap.end == self.capacity() {
+                return None;
+            }
+            let element = unsafe {
+                std::ptr::read(self.space(self.gap.end))
+            };
+            self.gap.end += 1;
+            Some(element)
+        }
+
+        fn enlarge_gap(&mut self) {
+            let mut new_capacity = self.capacity() * 2;
+            if new_capacity == 0 {
+                new_capacity = 4;
+            }
+
+            let mut new = Vec::with_capacity(new_capacity);
+            let after_gap = self.capacity() - self.gap.end;
+            let new_gap = self.gap.start .. new.capacity() - after_gap;
+
+            unsafe {
+                std::ptr::copy_nonoverlapping(self.space(0),
+                                                new.as_mut_ptr(),
+                                                    self.gap.start);
+
+                let new_gap_end = new.as_mut_ptr().offset(new_gap.end as isize);
+                std::ptr::copy_nonoverlapping(self.space(self.gap.end),
+                                                new_gap_end, after_gap);
+            }
+
+            self.storage = new;
+            self.gap = new_gap;
+        }
+    }
+
+    impl<T> Drop for GapBuffer<T> {
+        fn drop(&mut self) {
+            unsafe {
+                for i in 0 .. self.gap.start {
+                    std::ptr::drop_in_place(self.space_mut(i));
+                }
+                for i in self.gap.end .. self.capacity() {
+                    drop(std::ptr::read(self.space(i)));
+                }
+            }
+        }
+    }
+
+    pub struct Iter<'a, T: 'a> {
+        buffer: &'a GapBuffer<T>,
+        pos: usize
+    }
+
+    impl<'a, T: 'a> Iterator for Iter<'a, T> {
+        type Item = &'a T;
+
+        fn next(&mut self) -> Option<&'a T> {
+            if self.pos >= self.buffer.len() {
+                None
+            } else {
+                self.pos += 1;
+                self.buffer.get(self.pos - 1)
+            }
+        }
+    }
+
+    impl<'a, T: 'a> IntoIterator for &'a GapBuffer<T> {
+        type Item = &'a T;
+        type IntoIter = Iter<'a, T>;
+
+        fn into_iter(self) -> Iter<'a, T> {
+            Iter { buffer: self, pos: 0 }
+        }
+    }
+
+    impl GapBuffer<char> {
+        pub fn get_string(&self) -> String {
+            let mut text = String::new();
+            text.extend(self);
+            text
+        }
+    }
+
+    use std::fmt;
+    use std::fmt::Formatter;
+
+    impl<T: fmt::Debug> fmt::Debug for GapBuffer<T> {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let indices = (0..self.gap.start).chain(self.gap.end .. self.capacity());
+            let elements = indices.map(|i| unsafe { &*self.space(i) });
+            f.debug_list().entries(elements).finish()
+        }
+    }
+}
+
+mod gap_tests {
+    use crate::gap::GapBuffer;
+
+    #[test]
+    fn test() {
+        let mut buf = GapBuffer::new();
+        buf.insert_iter("Lord of the Rings".chars());
+        buf.set_position(12);
+
+        buf.insert_iter("Onion ".chars());
+
+        assert_eq!(buf.get_string(), "Lord of the Onion Rings");
+    }
+
+    #[test]
+    fn misc() {
+
+        let mut gb = GapBuffer::new();
+        println!("{:?}", gb);
+        gb.insert("foo".to_string());
+        println!("{:?}", gb);
+        gb.insert("bar".to_string());
+        println!("{:?}", gb);
+        gb.insert("baz".to_string());
+        println!("{:?}", gb);
+        gb.insert("qux".to_string());
+        println!("{:?}", gb);
+        gb.insert("quux".to_string());
+        println!("{:?}", gb);
+
+        gb.set_position(2);
+
+        assert_eq!(gb.remove(), Some("baz".to_string()));
+        println!("{:?}", gb);
+        assert_eq!(gb.remove(), Some("qux".to_string()));
+        println!("{:?}", gb);
+        assert_eq!(gb.remove(), Some("quux".to_string()));
+        println!("{:?}", gb);
+        assert_eq!(gb.remove(), None);
+        println!("{:?}", gb);
+
+        gb.insert("quuux".to_string());
+        println!("{:?}", gb);
+
+        gb.set_position(0);
+        assert_eq!(gb.remove(), Some("foo".to_string()));
+        println!("{:?}", gb);
+        assert_eq!(gb.remove(), Some("bar".to_string()));
+        println!("{:?}", gb);
+        assert_eq!(gb.remove(), Some("quuux".to_string()));
+        println!("{:?}", gb);
+        assert_eq!(gb.remove(), None);
+        println!("{:?}", gb);
+    }
+
+    #[test]
+    fn drop_elements() {
+
+        let mut gb = GapBuffer::new();
+        gb.insert("foo".to_string());
+        gb.insert("bar".to_string());
+
+        gb.set_position(1);
+    }
+}
