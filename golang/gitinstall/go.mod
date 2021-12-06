97fb433983fc5938bb35d3a79298f65da3422482
hushui502 <502131010@qq.com>
2021-12-06 19:27:35 +0800

    an installation for GitHub Apps

---
+++ golang/gitinstall/go.mod
@@ -0,0 +1,9 @@
+module gitinstall
+
+go 1.16
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.2.0
+	github.com/google/go-cmp v0.5.6
+	github.com/google/go-github/v40 v40.0.0
+)
---
+++ golang/gitinstall/installation_test.go
@@ -0,0 +1,106 @@
+package gitinstall
+
+import (
+	"bytes"
+	"fmt"
+	jwt "github.com/golang-jwt/jwt/v4"
+	"github.com/google/go-cmp/cmp"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestNewAppsTransportKeyFromFile(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "example")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	if _, err := tmpfile.Write(key); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = NewAppsTransportKeyFromFile(&http.Transport{}, appID, tmpfile.Name())
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+}
+
+type RoundTrip struct {
+	rt func(*http.Request) (*http.Response, error)
+}
+
+func (r RoundTrip) RoundTrip(req *http.Request) (*http.Response, error) {
+	return r.rt(req)
+}
+
+func TestAppsTransport(t *testing.T) {
+	customHeader := "my-header"
+	check := RoundTrip{
+		rt: func(req *http.Request) (*http.Response, error) {
+			h, ok := req.Header["Accept"]
+			if !ok {
+				t.Error("Header Accept not set")
+			}
+			want := []string{customHeader, acceptHeader}
+			if diff := cmp.Diff(want, h); diff != "" {
+				t.Errorf("HTTP Accept headers want->got: %s", diff)
+			}
+			return nil, nil
+		},
+	}
+
+	tr, err := NewAppsTransport(check, appID, key)
+	if err != nil {
+		t.Fatalf("error creating transport: %v", err)
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "http://example.com", new(bytes.Buffer))
+	req.Header.Add("Accept", customHeader)
+	if _, err := tr.RoundTrip(req); err != nil {
+		t.Fatalf("error calling RoundTrip: %v", err)
+	}
+}
+
+func TestJWTExpiry(t *testing.T) {
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	customHeader := "my-header"
+	check := RoundTrip{
+		rt: func(req *http.Request) (*http.Response, error) {
+			token := strings.Fields(req.Header.Get("Authorization"))[1]
+			tok, err := jwt.ParseWithClaims(token, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+				if t.Header["alg"] != "RS256" {
+					return nil, fmt.Errorf("unexpected signing method: %v, expected: %v", t.Header["alg"], "RS256")
+				}
+				return &key.PublicKey, nil
+			})
+			if err != nil {
+				t.Fatalf("jwt parse: %v", err)
+			}
+
+			c := tok.Claims.(*jwt.StandardClaims)
+			if c.ExpiresAt == 0 {
+				t.Fatalf("missing exp claim")
+			}
+			return nil, nil
+		},
+	}
+
+	tr := NewAppsTransportFromPrivateKey(check, appID, key)
+	req := httptest.NewRequest(http.MethodGet, "http://example.com", new(bytes.Buffer))
+	req.Header.Add("Accept", customHeader)
+	if _, err := tr.RoundTrip(req); err != nil {
+		t.Fatalf("error calling RoundTrip: %v", err)
+	}
+}
