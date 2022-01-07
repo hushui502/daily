19665a10ff06c8e9d80f4814ba590b8f79f76d5c
hushui502 <hushui502@gmail.com>
2022-01-07 13:08:38 +0800

    mem-align output |pretty

---
+++ golang/memalign/mem_test.go
@@ -0,0 +1,22 @@
+package memalign
+
+import "testing"
+
+type Car struct {
+	flag        bool
+	age         int32
+	F1          int8
+	F2          int64
+	F3          *int32
+	InnerStruct struct {
+		InnerByte byte
+		//InnerStr string
+	}
+	F4   []byte
+	Name string
+	F5   error
+}
+
+func TestColorFormatPrint(t *testing.T) {
+	PrintStructAlignment(Car{})
+}
