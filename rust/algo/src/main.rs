8910e2510bbf141fecf85bb09292b7c1efc8ca99
hushui502 <hushui502@gmail.com>
2022-04-21 22:35:23 +0800

    lcs

--- rust/algo/src/main.rs
+++ rust/algo/src/main.rs
@@ -1,3 +1,210 @@
+use std::collections::VecDeque;
+
 fn main() {
     println!("Hello, world!");
+}
+
+pub fn move_zeroes(nums: &mut Vec<i32>) {
+    let mut i = 0;
+    let mut j = 0;
+    while i < nums.len() {
+        if nums[i] != 0 {
+            nums.swap(i, j);
+            j += 1;
+        }
+        i += 1;
+    }
+}
+
+pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+    for i in (0..digits.len()).rev() {
+        if digits[i] != 9 {
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] = 0;
+        if i == 0 {
+            digits.insert(0, 1);
+        }
+    }
+    digits
+}
+
+pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    if nums.len() == 0 {
+        return 0;
+    }
+    let mut i = 0;
+    for j in 1..nums.len() {
+        if nums[i] != nums[j] {
+            if j - i > 1 {
+                nums[i + 1] = nums[j]
+            }
+            i += 1;
+        }
+    }
+
+    (i + 1) as i32
+}
+
+struct MinStack {
+    stack: Vec<i32>,
+    min_stack: Vec<i32>,
+}
+
+impl MinStack {
+    fn new() -> Self {
+        MinStack {
+            stack: Vec::new(),
+            min_stack: Vec::new(),
+        }
+    }
+
+    fn push(&mut self, x: i32) {
+        self.stack.push(x);
+        if self.min_stack.is_empty() || x <= self.min_stack[self.min_stack.len() - 1] {
+            self.min_stack.push(x);
+        }
+    }
+
+    fn pop(&mut self) {
+        if self.stack.pop().unwrap() == *self.min_stack.last().unwrap() {
+            self.min_stack.pop();
+        }
+    }
+
+    fn top(&self) -> i32 {
+        self.stack[self.stack.len() - 1]
+    }
+
+    fn get_min(&self) -> i32 {
+        self.min_stack[self.min_stack.len() - 1]
+    }
+}
+
+fn is_valid(s: String) -> bool {
+    let mut stack = Vec::new();
+    for c in s.chars() {
+        match c {
+            '(' | '{' | '[' => {
+                stack.push(c);
+            }
+            ')' => {
+                if stack.pop().unwrap() != '(' {
+                    return false;
+                }
+            }
+            '}' => {
+                if stack.pop().unwrap() != '{' {
+                    return false;
+                }
+            }
+            ']' => {
+                if stack.pop().unwrap() != '[' {
+                    return false;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    stack.is_empty()
+}
+
+pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    if nums.len() == 0 || k == 1 {
+        return nums;
+    }
+
+    let mut res: Vec<i32> = Vec::new();
+    let mut deque: VecDeque<i32> = VecDeque::new();
+    for i in 0..nums.len() {
+        push(&mut deque, nums[i]);
+        if (i as i32) > k - 1 {
+            pop(&mut deque, nums[i - k as usize]);
+            res.push(max(&deque));
+        } else if (i as i32) == k - 1 {
+            res.push(max(&deque));
+        }
+    }
+
+    res
+}
+
+pub fn push(deque: &mut VecDeque<i32>, num: i32) {
+    while !deque.is_empty() && *deque.back().unwrap() < num {
+        deque.pop_back();
+    }
+
+    deque.push_back(num);
+}
+
+pub fn pop(deque: &mut VecDeque<i32>, n: i32) {
+    if !deque.is_empty() && *deque.front().unwrap() == n {
+        deque.pop_front();
+    }
+}
+
+fn max(deque: &VecDeque<i32>) -> i32 {
+    if deque.is_empty() {
+        return 0;
+    }
+
+    *deque.front().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_move_zeroes() {
+        let mut nums = vec![0, 1, 0, 3, 12];
+        move_zeroes(&mut nums);
+        assert_eq!(nums, vec![1, 3, 12, 0, 0]);
+    }
+
+    #[test]
+    fn test_plus_one() {
+        assert_eq!(plus_one(vec![1, 2, 3]), vec![1, 2, 4]);
+        assert_eq!(plus_one(vec![4, 3, 2, 1]), vec![4, 3, 2, 2]);
+        assert_eq!(plus_one(vec![9, 9, 9, 9]), vec![1, 0, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_remove_duplicates() {
+        let mut nums = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+        assert_eq!(remove_duplicates(&mut nums), 5);
+        // assert_eq!(nums, vec![0, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_min_stack() {
+        let mut min_stack = MinStack::new();
+        min_stack.push(2);
+        min_stack.push(0);
+        min_stack.push(3);
+        min_stack.push(0);
+        assert_eq!(min_stack.get_min(), 0);
+        min_stack.pop();
+        assert_eq!(min_stack.top(), 3);
+        assert_eq!(min_stack.get_min(), 0);
+    }
+
+    #[test]
+    fn test_is_valid() {
+        assert_eq!(is_valid("()".to_string()), true);
+        assert_eq!(is_valid("()[]{}".to_string()), true);
+        assert_eq!(is_valid("(]".to_string()), false);
+        assert_eq!(is_valid("([)]".to_string()), false);
+        assert_eq!(is_valid("{[]}".to_string()), true);
+    }
+
+    #[test]
+    fn test_max_sliding_window() {
+        assert_eq!(
+            max_sliding_window(vec![1, 3, -1, -3, 5, 3, 6, 7], 3),
+            vec![3, 3, 5, 5, 6, 7]
+        );
+    }
 }
